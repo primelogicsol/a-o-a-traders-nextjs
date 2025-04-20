88d3bdf2d248b2fa22cc89d467fd1eee57851d8b
Abdullah <abdullah@prodoo.com>
2025-04-20 15:55:34 +0500

    init commit

---
+++ src/components/Header/Topbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Topbar: React.FC = () => {
+    return (
+        <div className="bg-gray-100 text-sm text-gray-700 py-2 border-b border-app_border">
+            <div className="container mx-auto px-4 flex justify-between items-center">
+                <div>Express Delivery</div>
+                <div className="flex items-center space-x-4">
+                    <a href="#" className="hover:underline">
+                        FAQ&apos;s
+                    </a>
+                    <span className="border-l border-gray-300 h-4"></span>
+                    <a href="#" className="hover:underline">
+                        Order Tracking
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Topbar;
---
+++ src/components/Header/menuData.ts
@@ -0,0 +1,40 @@
+import { Menu } from "@/types/Menu";
+
+export const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    newTab: false,
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Collection",
+    newTab: false,
+    path: "/shop-with-sidebar",
+  },
+  {
+    id: 3,
+    title: "All Products",
+    newTab: false,
+    path: "/contact",
+  },
+  {
+    id: 6,
+    title: "Blog",
+    newTab: false,
+    path: "/",
+  },
+  {
+    id: 7,
+    title: "About Us",
+    newTab: false,
+    path: "/",
+  },
+  {
+    id: 7,
+    title: "Contact Us",
+    newTab: false,
+    path: "/",
+  },
+];
